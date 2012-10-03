bc019616f30d371cb7dc7842a54b68c2d4f24343
Rajiv Subramanian M <rajiv.m1991@gmail.com>
2012-10-03 20:22:05 +0530

    updated readme file

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Rajiv-Py-SMS
 An easy to use Python module for sending SMS.
 
 This module currently supports sending sms through Way2Sms.
+
+Usage:
+======
+    from RajivPySms import RajivSmsModule
+    
+    MyPager = RajivSmsModule()
+    
+    MyPager.login(UNAME='9876543210',PWD='way2smsPassword')
+    
+    MyPager.set_signature(" ~ By Rajiv")
+    
+    MyPager.send(RECEIVER = "9952113011", MESSAGE = "Good Morning, Have a nice day...")
--- sample_application.py
+++ sample_application.py
@@ -1,7 +1,52 @@
 #!/usr/bin/python
-from RajivPySms import *
+from RajivPySms import RajivSmsModule
 
-if __name__ == "__main__":
+def send_instant_sms(MyPager, RECEIVER=False, MESSAGE=False):
+    'instant sms to a receiver'
+    if RECEIVER and MESSAGE: MyPager.send(RECEIVER,MESSAGE)
+    else:
+        RECEIVER    = raw_input("Receiver no: ")
+        MESSAGE     = raw_input("Your msg: ")
+        MyPager.send(RECEIVER,MESSAGE)
+
+def start_one_to_one_chat(MyPager, RECEIVER=False):
+    'Its like sms chat, simultaneous message to single person'
+    if not RECEIVER: RECEIVER = raw_input("Receiver no: ")
+    begun = 'y'
+    while begun == 'y':
+        MESSAGE = raw_input("Your msg: ")
+        MyPager.send(RECEIVER,MESSAGE)
+        begun = raw_input("Want send another sms(y/n): ")
+
+def send_group_sms(MyPager, RECEIVERS=False, CONTACT_CSV_FILE=False, MESSAGE=False):
+    'Its like group sms: Send a message to "comma seperated numbers" OR "all contacts in a csv file"'
+    if not CONTACT_CSV_FILE:
+        if not RECEIVERS:
+            RECEIVERS = raw_input("Receivers no(comma seperated): ")
+    else:
+        RECEIVERS = ''
+        for contact in open(CONTACT_CSV_FILE,'r'):
+            RECEIVERS += contact.split(',')[1]+','
+
+    if not MESSAGE:
+        MESSAGE = raw_input("Your msg: ")
+    #======================= confirm before sending  ================
+    size,parts,msg = MyPager.check_message_size(MESSAGE)
+    print "message size: %d chars"%(size)
+    if parts != 1:
+        print "split into: %d parts"%(parts)
+    print "your message: %s"%(msg)
+    permit = raw_input("continue?(y/n) :")
+    if permit == 'n':
+        print "! sending process stopped.."
+        return False
+    #================================================================
+
+    for RECEIVER in RECEIVERS.split(','):
+        MyPager.send(RECEIVER,MESSAGE)
+    return True
+
+def main():
     #======================= You can Run this program as a Stand Alone App ===========================
     MyPager = RajivSmsModule()
     option = '0'
@@ -27,4 +72,7 @@ if __name__ == "__main__":
         elif option == 's': MyPager.set_signature(raw_input("enter your new sign: "));print "success!!"
         elif option == 'l': MyPager.login()
         elif option != 'x': print 'invalid option'
-        
+
+
+if __name__ == "__main__":
+    main()

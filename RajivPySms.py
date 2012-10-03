77788208789f7d166686bed9f856557e4329e0fe
Rajiv Subramanian M <rajiv.m1991@gmail.com>
2012-10-03 19:49:57 +0530

    updated main module

--- RajivPySms.py
+++ RajivPySms.py
@@ -78,6 +78,7 @@ class RajivSmsModule:
         else:
             print "Your message cannot be sent. Please login first :P"
             return False
+
 def print_browser_response(browser):
     print "++++++++++++++++++++ browser response +++++++++++++++++++"
     response = browser.response().info()
@@ -131,32 +132,4 @@ def send_group_sms(MyPager, RECEIVERS=False, CONTACT_CSV_FILE=False, MESSAGE=Fal
     for RECEIVER in RECEIVERS.split(','):
         MyPager.send(RECEIVER,MESSAGE)
     return True
-
-if __name__ == "__main__":
-    #======================= If run as Stand Alone App ===========================
-    MyPager = RajivSmsModule()
-    option = '0'
-    while option != 'x':
-        print """
-+---+------+------+------+------+------+------+
-|                    Options                  |
-| i - Instant SMS                             |
-| c - Chat                                    |
-| g - Group SMS                               |
-| a - Sms to all contacts in sms_contact.csv  |
-| s - set/change signature                    | (current sign: %s)
-| l - login/re-login                          | (current user: %s)
-| x - Exit                                    |
-+---+------+------+------+------+------+------+
-"""%(MyPager.get_signature(),MyPager.get_user())
-        option = raw_input('Select your option: ')
-        if   option == 'i': send_instant_sms(MyPager)
-        elif option == 'c': start_one_to_one_chat(MyPager)
-        elif option == 'g': send_group_sms(MyPager)
-        elif option == 'a': send_group_sms(MyPager,CONTACT_CSV_FILE="sms_contact.csv")
-        elif option == 's': MyPager.set_signature(raw_input("enter your new sign: "));print "success!!"
-        elif option == 's': MyPager.set_signature(raw_input("enter your new sign: "));print "success!!"
-        elif option == 'l': MyPager.login()
-        elif option != 'x': print 'invalid option'
-
 

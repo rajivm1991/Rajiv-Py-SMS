84a413b49a2826ca937d5efe4d34a6d190138336
Rajiv Subramanian M <rajiv.m1991@gmail.com>
2012-10-04 10:52:57 +0530

    confirm before sending option included

--- RajivPySms.py
+++ RajivPySms.py
@@ -5,6 +5,7 @@ import getpass
 class RajivSmsModule:
     HIDDEN, ACTION, CATEGORY = "instantsms", "sa65sdf656fdfd", "Friendship+Day"
     SIGNATURE, USER = "", ""
+    SPLIT_OR_TRUNCATE = True
     Login_status = False
     def __init__(self):
         from mechanize import Browser
@@ -23,6 +24,7 @@ class RajivSmsModule:
         #self.browser.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10')]
         
         self.browser.method = "POST"
+        
     def login(self,UNAME='',PWD=''):
         if not UNAME: UNAME = raw_input("Enter Your Mobile number: ")
         if not PWD:   PWD   = getpass.getpass("Enter Your Password: ")
@@ -37,46 +39,64 @@ class RajivSmsModule:
         print "+|",self.browser.geturl().split('/')[-1].split('action;')[-1].replace('?','\n+| ').replace('=',' : ')
         print "+++++++++++++++++++++++++++++++++++++++++++"
         return self.Login_status
+
     def set_signature(self, SIGNATURE):
         self.SIGNATURE = SIGNATURE.replace(' ','+')
+
     def get_signature(self):
         return self.SIGNATURE.replace('+',' ')
+
     def get_user(self):
         return self.USER if self.USER else "Nobody logged in"
+
     def check_message_size(self, MESSAGE):
-        MESSAGE = MESSAGE + self.SIGNATURE.replace('+',' ')
+        MESSAGE = MESSAGE.replace('&','and').strip() + self.SIGNATURE.replace('+',' ')
         parts = 1 if len(MESSAGE)<130 else ( (len(MESSAGE)/114) + (1 if len(MESSAGE)%114 else 0) )
         return len(MESSAGE),parts,MESSAGE
-    def send(self,RECEIVER,MESSAGE):
+
+    def send(self,RECEIVER,MESSAGE,CONFIRM_BEFORE_SENDING = False):
         RECEIVER, MESSAGE = RECEIVER.strip(), MESSAGE.strip()
         if self.Login_status:
-            if len(RECEIVER) == 10:
-                print "sending msg to %s..."%(RECEIVER)
-                MESSAGE = MESSAGE.replace('&','and') + self.SIGNATURE
-                MSG_list = []
-                if len(MESSAGE) > 130:
-                    MESSAGE = fill(MESSAGE, width=114, fix_sentence_endings=True).split('\n')
-                    MSG_list = [ (MESSAGE[i]+' [part %0.2d of %0.2d]'%(i+1,len(MESSAGE))).replace(' ','+') for i in range(len(MESSAGE)) ]
+            if MESSAGE == '':
+                if len(RECEIVER) == 10:
+                    length,parts,final_msg = self.check_message_size(MESSAGE)
+                    if CONFIRM_BEFORE_SENDING and parts > 1:
+                        allow = raw_input("your message will be splitted into %2d parts, do you want to proceed(y/n): "%(parts) if self.SPLIT_OR_TRUNCATE else "your message is %d long, it will be truncated to 130 chars, do you want to proceed sending(y/n): "%(length))
+                        if allow == 'n':
+                            print "! sending process stopped.."
+                            return False
+                    print "sending msg to %s..."%(RECEIVER)
+                    MESSAGE = final_msg.replace(' ','+')
+                    MSG_list = []
+                    if parts == 1:
+                        MSG_list.append( MESSAGE )
+                    else:
+                        if self.SPLIT_OR_TRUNCATE:
+                            MESSAGE = fill(MESSAGE, width=114, fix_sentence_endings=True).split('\n')
+                            MSG_list = [ (MESSAGE[i]+' [part %0.2d of %0.2d]'%(i+1,len(MESSAGE))).replace(' ','+') for i in range(len(MESSAGE)) ]
+                        else:
+                            MSG_list.append( MESSAGE[:130].replace(' ','+') )
+                    try:
+                        part,total = 1,len(MSG_list)
+                        for MSG in MSG_list:
+                            self.browser.open("http://site2.way2sms.com/quicksms.action?MobNo="+RECEIVER+"&textArea="+MSG+"&HiddenAction="+self.HIDDEN+"&Action="+self.ACTION+"&catnamedis="+self.CATEGORY)
+                            if total>1:
+                                print '\tpart',part,'of',total,'sent'
+                                part += 1
+                            sleep(1)
+                        status = print_browser_response(self.browser)
+                        return status
+                    except:
+                        print "!Unable to send your message :P"
+                        return False
                 else:
-                    MSG_list.append( MESSAGE.replace(' ','+') )
-                try:
-                    part,total = 1,len(MSG_list)
-                    for MSG in MSG_list:
-                        self.browser.open("http://site2.way2sms.com/quicksms.action?MobNo="+RECEIVER+"&textArea="+MSG+"&HiddenAction="+self.HIDDEN+"&Action="+self.ACTION+"&catnamedis="+self.CATEGORY)
-                        if total>1:
-                            print '\tpart',part,'of',total,'sent'
-                            part += 1
-                        sleep(1)
-                    status = print_browser_response(self.browser)
-                    return status
-                except:
-                    print "Unable to send your message :P"
+                    print "!Message cannot be sent to this receiver %s :P"%(RECEIVER)
                     return False
             else:
-                print "Message cannot be sent to this receiver %s :P"%(RECEIVER)
+                print "!No content in your message.."
                 return False
         else:
-            print "Your message cannot be sent. Please login first :P"
+            print "!Please login first :P"
             return False
 
 def print_browser_response(browser):

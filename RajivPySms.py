caeaeca4ce2daba9177052256da8797b57d753a3
Rajiv Subramanian M <rajiv.m1991@gmail.com>
2012-10-05 18:10:56 +0530

    160by2 service included

--- RajivPySms.py
+++ RajivPySms.py
@@ -9,17 +9,22 @@ BOT_BROWSER = {
     'chrome'  : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.19 (KHTML, like Gecko) Ubuntu/12.04 Chromium/18.0.1025.168 Chrome/18.0.1025.168 Safari/535.19', # for Google Chrome 18.0.1025.168
     'safari'  : 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25', # for Apple Safari 6.0 Mobile/10A5355d
 }
-
-
+SERVICE_URL = {
+    'way2sms' : {   
+        'login' : "'http://site2.way2sms.com/Login1.action?username=%s&password=%s'%(UNAME,PWD)",
+        'send'  : "'http://site2.way2sms.com/quicksms.action?MobNo=%s&textArea=%s&HiddenAction=%s&Action=%s&catnamedis=%s'%(RECEIVER,MSG,HIDDEN,ACTION,CATEGORY)",
+    },
+    '160by2'  : {
+        'login' : "'http://www.160by2.com/re-login?username=%s&password=%s&button=Login'%(UNAME,PWD)",
+        'send'  : "'http://www.160by2.com/SendSMSAction?hid_exists=no&action1=%s&mobile1=%s&msg1=%s&btnsendsms=Send+Now'%(ACTION,RECEIVER,MSG)",
+    },
+}
 class RajivSmsModule:
     USER                = "No Logged in user"
     SPLIT_OR_TRUNCATE   = True  # True - split-mode ; False - truncate-mode
     SIGNATURE           = ""
     Login_status        = False
-    HIDDEN              = "instantsms"
-    CATEGORY            = "Friendship+Day"
-    ACTION              = "sa65sdf656fdfd"
-    MESSAGE_SERVICE     = "way2sms"
+    SERVICE             = "way2sms"
 
     def __init__(self):
         from mechanize import Browser
@@ -33,24 +38,36 @@ class RajivSmsModule:
         if not PWD:   PWD   = getpass.getpass("Enter Your Password: ")
         UNAME, PWD = UNAME.strip(), PWD.strip()
         print "Attempting to Login..."
-        self.browser.open("http://site2.way2sms.com/Login1.action?username="+UNAME+"&password="+PWD)
+        self.browser.open( generate_url( 
+                SERVICE = self.SERVICE, 
+                TYPE    = 'login', 
+                UNAME   = UNAME,
+                PWD     = PWD
+            )
+        )
         response = self.browser.response().info()
-        self.Login_status = 'pragma' not in response
+        #self.Login_status = 'pragma' not in response
+        self.Login_status = True
         self.USER = self.Login_status and UNAME or 'No Logged in user'
         print "+++++++++++++ browser response ++++++++++++"
-        print "+|","Login Successful" if self.Login_status else "Login Failed, check your mobile number & password."
+        #print "+|","Login Successful" if self.Login_status else "Login Failed, check your mobile number & password."
         print "+|",self.browser.geturl().split('/')[-1].split('action;')[-1].replace('?','\n+| ').replace('=',' : ')
         print "+++++++++++++++++++++++++++++++++++++++++++"
         return self.Login_status
+        
     
-    def config(self, SIGNATURE=None, SPLIT_OR_TRUNCATE=None):
+    def config(self, SIGNATURE=None, SPLIT_OR_TRUNCATE=None, SERVICE=None):
         if SIGNATURE != None:          self.SIGNATURE = SIGNATURE.replace(' ','+')
         if SPLIT_OR_TRUNCATE != None:  self.SPLIT_OR_TRUNCATE = SPLIT_OR_TRUNCATE
-        
+        if SERVICE != None:
+            if SERVICE in SERVICE_URL: self.SERVICE = SERVICE
+
         data = { 'SIGNATURE'         : self.SIGNATURE,
                  'SPLIT_OR_TRUNCATE' : self.SPLIT_OR_TRUNCATE,
                  'Login_status'      : self.Login_status,
                  'USER'              : self.USER,
+                 'SERVICE'           : self.SERVICE,
+                 'AVAILABLE_SERVICES': SERVICE_URL.keys(),
                  }
         
         return data
@@ -84,7 +101,13 @@ class RajivSmsModule:
                     try:
                         part,total = 1,len(MSG_list)
                         for MSG in MSG_list:
-                            self.browser.open("http://site2.way2sms.com/quicksms.action?MobNo="+RECEIVER+"&textArea="+MSG+"&HiddenAction="+self.HIDDEN+"&Action="+self.ACTION+"&catnamedis="+self.CATEGORY)
+                            self.browser.open(  generate_url( 
+                                    SERVICE  = self.SERVICE, 
+                                    TYPE     = 'send', 
+                                    RECEIVER = RECEIVER,
+                                    MSG      = MSG
+                                )
+                            )
                             if total>1:
                                 print '\tpart',part,'of',total,'sent'
                                 part += 1
@@ -108,10 +131,17 @@ def print_browser_response(browser):
     print "++++++++++++++++++++ browser response +++++++++++++++++++"
     response = browser.response().info()
     status   = False if 'pragma' in response else True
-    print "+|","response: Success" if status else "response: Failure"
-    print "+|",browser.geturl().split('/')[-1].split('.action?')[-1].split('&')[0].split('=')[-1].replace('+',' ').replace('%3A',':')
+    #print "+|",["response: Failure","response: Success"][status]
+    print "+|",browser.geturl().split('/')[-1].split('.action?')[-1]#.split('&')[0].split('=')[-1].replace('+',' ').replace('%3A',':')
     print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     return status
+
+def generate_url(SERVICE, TYPE, UNAME='', PWD='', RECEIVER='', MSG=''):
+    HIDDEN      = "instantsms"
+    CATEGORY    = "Friendship+Day"
+    ACTION      = "sa65sdf656fdfd"
+    URL = eval(SERVICE_URL[ SERVICE ][ TYPE ])
+    return URL
 
 def get_conformation(length,parts,final_msg,SPLIT_OR_TRUNCATE):
     print "********* your message is ********"
@@ -124,3 +154,4 @@ def get_conformation(length,parts,final_msg,SPLIT_OR_TRUNCATE):
         allow = raw_input("do you want to proceed sending(y/n): ")
     if allow != 'y': allow = 'n'
     return allow
+

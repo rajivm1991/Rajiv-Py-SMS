ec4ca6ff8aff26cde1c781511870485bb2ac0960
Rajiv Subramanian M <rajiv.m1991@gmail.com>
2012-10-11 13:09:39 +0530

    message style changes

--- RajivPearlsAddon.py
+++ RajivPearlsAddon.py
@@ -7,7 +7,7 @@ browser.addheaders = [('User-agent', BOT_BROWSER['safari'] )]
 date = datetime.strftime(datetime.now(),"%B %d, %I:%M %p")
 
 def get_forex_rate(From = 'USD', To = 'INR'):
-    msg = "Rajiv-Pearl-Forex%0a"+date+"%0a==========%0a"
+    msg = "Rajiv-Pearl-Forex%0a"+date+"%0a----------%0a"
     data = browser.open("http://www.xe.com/ucc/convert.cgi?template=mobile&Amount=1&From="+From+"&To="+To).read()
     n = data.index('1 '+From+' =')
     m = data[n:].index(' '+To)
@@ -15,7 +15,7 @@ def get_forex_rate(From = 'USD', To = 'INR'):
     n = data.index('1 '+To+' =')
     m = data[n:].index(' '+From)
     msg += data[n:n+m+4]
-    msg += "%0a=========="
+    msg += "%0a----------"
     
     print "+--+----+--Pearl Response---+----+----+"
     print msg.replace('%0a','\n')
